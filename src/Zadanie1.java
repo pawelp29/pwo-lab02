7634c8f3b2fd1c3e8632806cdd432fdae708fb9f
Paweł Pańczyk <pawel.panczyk@pollub.edu.pl>
2022-10-12 19:07:28 +0200

    Zadanie 1 - wersja 1 + poprawka

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -10,7 +10,7 @@ public class Zadanie1 {
         System.out.println("Fibonacci wersja 1");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i <= 100; i++)
                 writer.write(Fibonacci(i).toString() + "\n");
             writer.close();
         } catch (IOException ex) {
